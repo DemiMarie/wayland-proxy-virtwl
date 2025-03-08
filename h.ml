aeebf7b30c5e8fb36de106fb727bc5d56363015f
Thomas Leonard <talex5@gmail.com>
2025-03-08 13:29:10 +0000

    Add cursor-shape protocol

--- h.ml
+++ h.ml
@@ -10,3 +10,4 @@ include Wayland_protocols.Xdg_decoration_unstable_v1_client
 include Wayland_protocols.Relative_pointer_unstable_v1_client
 include Wayland_protocols.Pointer_constraints_unstable_v1_client
 include Wayland_protocols.Viewporter_client
+include Wayland_protocols.Cursor_shape_v1_client
